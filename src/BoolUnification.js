058689d46e57aeef08f467b85694e7cf589438cf
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-03-08 10:16:11 +0100

    Don't use toString.

--- src/BoolUnification.js
+++ src/BoolUnification.js
@@ -113,7 +113,7 @@ function satisfiable(f) {
     } else if (isFalse(f)) {
         return false
     } else {
-        throw new Error(`Illegal argument 'f': ${f.toString()}.`)
+        throw new Error(`Illegal argument 'f': ${f}.`)
     }
 }
 
